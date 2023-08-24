5b0f7b83d3492e7c7c453177a089281c3d9729b6
ravibatt <42490171+ravibatt@users.noreply.github.com>
2023-08-24 12:22:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.title('🥣 Omega 3 and Blue Berry Oatmeal')
 streamlit.title('🥗 Kale, Spinach, and Rocket Smoothe')
 streamlit.title('🐔 Hard-Boiled Free Range Egg')
 streamlit.title('🥑🍞 Avacado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
