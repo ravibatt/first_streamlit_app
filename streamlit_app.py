7756f34cbd14d2cc5d9c126b7f203fb466a5a0cd
ravibatt <42490171+ravibatt@users.noreply.github.com>
2023-08-24 12:21:00 +0530

    Update streamlit_app.py
    Added Emojis and text in 4th line

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Menu')
 
-streamlit.title('Omega 3 and Blue Berry Oatmeal')
-streamlit.title('Kale, Spinach, and Rocket Smoothe')
-streamlit.title('Hard-Boiled Free Range Egg')
+streamlit.title('🥣 Omega 3 and Blue Berry Oatmeal')
+streamlit.title('🥗 Kale, Spinach, and Rocket Smoothe')
+streamlit.title('🐔 Hard-Boiled Free Range Egg')
+streamlit.title('🥑🍞 Avacado toast')
